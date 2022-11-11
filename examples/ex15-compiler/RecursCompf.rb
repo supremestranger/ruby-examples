95da729f9137013799e7e67d76974ea2e0e576c2
Arthur <37938234+supremestranger@users.noreply.github.com>
2022-11-11 22:20:27 +0300

    * Remove extra line.

--- examples/ex15-compiler/RecursCompf.rb
+++ examples/ex15-compiler/RecursCompf.rb
@@ -3,7 +3,6 @@
 class RecursComp
   Parentheses = ['(', '[', '{']
   HighPriorityOperators = ['*', '/', '%']
-  Comments = ["//", "/*", "*/"]
 
   def compile(str)
     @str,@index = preprocess(str),0
